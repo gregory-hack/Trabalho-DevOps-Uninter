c3fc78ac2643b5e9f17c5ffe9cb0a8005f570cb4
gregory-hack <gregoryhack@hotmail.com>
2026-08-15 19:09:32 -0300

    feat personalização da mensagem de boas-vindas

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def boas_vindas(nome):
     agora = datetime.now().strftime("%d/%m/%Y %H:%M")
-    print(f"Olá, {nome}! Bem-vindo à CodeFactory Solutions.")
+    print(f"Olá, {nome}! Bem-vindo à Equipe de desenvolvimento da Code Factory.")
     print(f"Ambiente configurado com sucesso em {agora}.")
 
 
