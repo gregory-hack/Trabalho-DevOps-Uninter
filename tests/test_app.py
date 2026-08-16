5aea95a6799b3c26b5f47f4141b446f9ce78b510
gregory-hack <gregoryhack@hotmail.com>
2026-08-15 22:26:13 -0300

    test: adiciona teste para despedida e cria gitignore

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,10 +3,18 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "app"))
 
-from app import boas_vindas
+from app import boas_vindas, despedida
 
 
 def test_boas_vindas_nao_gera_erro(capsys):
     boas_vindas("Aluno Teste")
     saida = capsys.readouterr().out
-    assert "Bem-vindo à CodeFactory Solutions" in saida
+    assert "Aluno Teste" in saida
+    assert len(saida) > 0
+
+
+def test_despedida_nao_gera_erro(capsys):
+    despedida("Aluno Teste")
+    saida = capsys.readouterr().out
+    assert "Aluno Teste" in saida
+    assert len(saida) > 0
